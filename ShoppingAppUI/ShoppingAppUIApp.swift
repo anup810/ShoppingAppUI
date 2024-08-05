b31ad23420e91444cd5144832f1f4df9528085bd
Anup Saud <anupsaud10@gmail.com>
2024-08-05 09:37:40 -0400

    Initial Commit

---
+++ ShoppingAppUI/ShoppingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppUIApp.swift
+//  ShoppingAppUI
+//
+//  Created by Anup Saud on 2024-08-05.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
